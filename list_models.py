61304e9757a719c42acc88c558430536e4a959d1
Jose Rafael Hernandez <joserafaelhernandez@macbookpro.mynetworksettings.com>
2025-05-20 16:39:02 -0400

    Initial commit

---
+++ list_models.py
@@ -0,0 +1,13 @@
+# list_models.py
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+models = client.models.list()
+
+for model in models.data:
+    print(model.id)
+    
